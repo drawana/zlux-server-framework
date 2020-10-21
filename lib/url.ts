7b85055a80918599ebe78ba170d9643f683bf562
Daniel Rawana <danielrawana@Daniels-MacBook-Pro.local>
2020-10-20 23:43:21 -0400

    url.js -> url.ts

--- lib/url.ts
+++ lib/url.ts
@@ -10,7 +10,7 @@
 */
 
 
-function makePluginURL(productCode, pluginID) {
+export function makePluginURL(productCode : string, pluginID : string) {
   return `/${productCode}/plugins/${pluginID}`;
 }
 
@@ -24,7 +24,7 @@ function makeServiceSubURL(service, latest, omitVersion) {
   if (omitVersion) {
     return `/services/${nameForURL}`;
   } else {
-    const version = latest? '_current' : service.version;
+    const version = latest ? '_current' : service.version;
     return `/services/${nameForURL}/${version}`;
   }
 }
@@ -44,8 +44,8 @@ module.exports = {
   This program and the accompanying materials are
   made available under the terms of the Eclipse Public License v2.0 which accompanies
   this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
+
   SPDX-License-Identifier: EPL-2.0
-  
+
   Copyright Contributors to the Zowe Project.
 */
